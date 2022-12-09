3b688b93eb585acf5e41aa4788bac5369c56150d
D-spacezero <112856412+D-spacezero@users.noreply.github.com>
2022-12-09 11:38:51 +0100

    toggle

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -12,3 +12,13 @@ function showQuestion(questionNum) {
   var question = document.getElementById("question-" + questionNum);
   question.style.display = "block";
 }
+
+// Get the button and the element
+var button = document.getElementById("bediening-toggle");
+var element = document.getElementById("bediening");
+
+// Add an event listener to the button
+button.addEventListener("click", function() {
+  // Toggle the class "active" on the element
+  element.classList.toggle("checklist-active-cat");
+});
